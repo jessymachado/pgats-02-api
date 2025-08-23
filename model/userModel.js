52b43cba5a3f6b7763afb27d0223bb066aa93bb3
Jessica Machado <jeh-machado@outlook.com>
2025-08-23 15:25:18 -0300

    🐛 Correção teste com falha

--- model/userModel.js
+++ model/userModel.js
@@ -1,11 +1,18 @@
-// In-memory user database
 const bcrypt = require('bcryptjs');
+
+// In-memory user database
 const users = [
   {
-    username: 'julio',
-    password: bcrypt.hashSync('123456', 8),
-    favorecidos: ['priscila'],
-    saldo: 1000
+    username: 'julio', 
+    password: bcrypt.hashSync('123456', 8), 
+    favorecidos: [ 'priscila' ], 
+    saldo: 10000
+  },
+  {
+    username: 'priscila', 
+    password: bcrypt.hashSync('123456', 8), 
+    favorecidos: [ 'julio' ], 
+    saldo: 10000
   }
 ];
 
